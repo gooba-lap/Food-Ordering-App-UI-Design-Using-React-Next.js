e96894926c0966175ce45785ef9e8ae0058dcccd
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-01-10 19:48:20 +0700

    Next.js Basics

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Food-Ordering-App-UI-Design-Using-React-Next.js
 from : https://www.youtube.com/watch?v=vIxGDq1SPZQ
+
+# set 
+
+npx create-next-app
+
+# run
+
+yarn dev
---
+++ restaurant/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+      <Head>
+        <title>Pizza Restaurant in Newyork</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      homepage
+    </div>
+  )
+}
